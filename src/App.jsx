0e0f4d37dd0803ebef702594a5fcf7c06a8effc7
Jettberg <jettmberg@gmail.com>
2026-01-15 09:46:14 -0800

    adding auth functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { getEvents, extractLocations } from './api'
 
 
 import EventList from './components/EventList';
@@ -7,12 +8,38 @@ import NumberOfEvents from './components/NumberOfEvents';
 import CitySearch from './components/CitySearch';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity,]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    console.log('allEvents:', allEvents);
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter(event => event.location === currentCity);
+
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
+
+
   return (
     <div>
       <div className="App">
         <NumberOfEvents />
-        <CitySearch />
-        <EventList />
+        <CitySearch
+          allLocations={allLocations}
+          setCurrentCity={setCurrentCity} />
+        <EventList
+          events={events} />
       </div>
     </div>
   );
