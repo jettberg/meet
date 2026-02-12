b81b6f363780eb0abc87b20163dacc621f71eb7a
Jettberg <jettmberg@gmail.com>
2026-02-12 09:14:24 -0800

    adding icons, a manifest.json and service workers

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 console.log('Initializing Atatus...');
 atatus.config('7b807a1af528491889095798cf77f29b').install();
 console.log('Atatus installed');
@@ -13,4 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();
