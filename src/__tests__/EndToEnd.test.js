53e72f0c32e51fb17703799ab134c36cfe86280d
Jettberg <jettmberg@gmail.com>
2026-02-18 15:40:38 -0600

    updated dependencies and tweaked alerts and endToEnd

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -2,6 +2,7 @@ import puppeteer from "puppeteer";
 
 describe("show/hide event details", () => {
     let browser;
+    let context;
     let page;
 
     beforeAll(async () => {
@@ -12,22 +13,19 @@ describe("show/hide event details", () => {
     });
 
     beforeEach(async () => {
-        const context = await browser.createIncognitoBrowserContext();
-        page = await browser.newPage();
-        await page.goto("http://localhost:3000/");
+        context = await browser.createIncognitoBrowserContext();
+        page = await context.newPage();
+        await page.goto("http://localhost:3000/", { waitUntil: "networkidle0" });
         await page.waitForSelector(".event");
     });
 
     afterEach(async () => {
-        if (page) {
-            await page.close();
-        }
+        if (page) await page.close();
+        if (context) await context.close();
     });
 
     afterAll(async () => {
-        if (browser) {
-            await browser.close();
-        }
+        if (browser) await browser.close();
     });
 
     test("An event element is collapsed by default", async () => {
@@ -48,58 +46,57 @@ describe("show/hide event details", () => {
         expect(eventDetails).toBeNull();
     });
 
-    describe("filter events by city", () => {
-        let browser;
-        let page;
+    // describe("filter events by city", () => {
+    //     let browser;
+    //     let page;
 
-        beforeAll(async () => {
-            browser = await puppeteer.launch({
-                headless: "new",
-                args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            });
-        });
+    //     beforeAll(async () => {
+    //         browser = await puppeteer.launch({
+    //             headless: "new",
+    //             args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    //         });
+    //     });
 
-        beforeEach(async () => {
-            page = await browser.newPage();
-            await page.goto("http://localhost:3000/");
-            await page.waitForSelector(".event");
-        });
+    //     beforeEach(async () => {
+    //         page = await browser.newPage();
+    //         await page.goto("http://localhost:3000/");
+    //         await page.waitForSelector(".event");
+    //     });
 
-        afterEach(async () => {
-            if (page) {
-                await page.close();
-            }
-        });
+    //     afterEach(async () => {
+    //         if (page) {
+    //             await page.close();
+    //         }
+    //     });
 
-        afterAll(async () => {
-            if (browser) {
-                await browser.close();
-            }
-        });
+    //     afterAll(async () => {
+    //         if (browser) {
+    //             await browser.close();
+    //         }
+    // });
 
-        test("When user hasn’t searched for a city, all events are shown", async () => {
-            const events = await page.$$(".event");
-            expect(events.length).toBeGreaterThan(0);
-        });
+    test("When user hasnt searched for a city, all events are shown", async () => {
+        const events = await page.$$(".event");
+        expect(events.length).toBeGreaterThan(0);
+    });
 
-        test("User can filter events by city", async () => {
-            const cityInputSelector = ".city input";
+    test("User can filter events by city", async () => {
+        const cityInputSelector = ".city input";
 
-            await page.waitForSelector(cityInputSelector);
-            await page.click(cityInputSelector);
-            await page.type(cityInputSelector, "Berlin");
+        await page.waitForSelector(cityInputSelector);
+        await page.click(cityInputSelector);
+        await page.type(cityInputSelector, "Berlin");
 
-            await page.waitForSelector(".event");
+        await page.waitForSelector(".event");
 
-            const events = await page.$$(".event");
+        const events = await page.$$(".event");
 
-            for (const event of events) {
-                const eventCity = await event.$eval(
-                    ".event-location",
-                    (node) => node.textContent
-                );
-                expect(eventCity).toContain("Berlin");
-            }
-        });
+        for (const event of events) {
+            const eventCity = await event.$eval(
+                ".event-location",
+                (node) => node.textContent
+            );
+            expect(eventCity).toContain("Berlin");
+        }
     });
 });
